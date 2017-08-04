00bace6e4083e21008260583ff009bff88b9b2f3
romanfederation <so.andrew0@gmail.com>
2017-08-03 21:38:25 -0400

    messageReactionAdd basic functionality

--- commands/about.js
+++ commands/about.js
@@ -1,4 +1,11 @@
+const Discord = require("discord.js");
+
 exports.run = (client, message) => {
     console.log("Command !%s received from %s", "about", message.author.username);
-    message.channel.send("__**Hussein Bot: \"Hi I live here\"**__\n\nCurrent version: `2.1.0`\n\nCreated by <@197598081867448320> using the Discord.js JavaScript library (Now with 200% more JavaScript)\nCredit to <@231973798436536322> for the idea and basic logic structure");
+    //message.channel.send("__**Hussein Bot: \"Hi I live here\"**__\n\nCurrent version: `2.1.0`\n\nCreated by <@197598081867448320> using the Discord.js JavaScript library (Now with 200% more JavaScript)\nCredit to <@231973798436536322> for the idea and basic logic structure");
+    const embed = new Discord.RichEmbed()
+        .setTitle("**Hussein Bot**")
+        .setDescription("\"Hi I live here\"\n\nCreated by <@197598081867448320> using the Discord.js JavaScript library (Now with 200% more JavaScript)\n\nCredit to <@231973798436536322> for the idea and basic logic structure")
+        .addField("Current Version", "2.2.0");
+    message.channel.send({embed: embed});
 };
--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,8 +1,19 @@
-exports.run = (client, message, messageReaction) => {
-    console.log(messageReaction.emoji.identifier);
+exports.run = (client, messageReaction) => {
+    //console.log(messageReaction.emoji.identifier);
     if(messageReaction.emoji.id === "310668328794587138"){
-        message.react("310668328794587138")
+        messageReaction.message.react("310668328794587138")
             .then(() => {})
             .catch(console.log)
     }
+    if(messageReaction.emoji.id === "310701468061794304"){
+        messageReaction.message.react("310701468061794304")
+            .then(() => {})
+            .catch(console.log)
+    }
+    if(messageReaction.emoji.identifier === "%F0%9F%A4%9C" || messageReaction.emoji.identifier === "%F0%9F%91%8A"){
+        messageReaction.message.react("%E2%9C%8B")
+            .then(() => {})
+            .catch(console.log)
+    }
+
 };
