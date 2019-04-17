33e91aba7bb5c3e014fef765ee8b9f00222f9211
Andrew So <so.andrew0@gmail.com>
2019-04-17 04:49:04 -0400

    Update version number

--- commands/about.js
+++ commands/about.js
@@ -13,7 +13,7 @@ module.exports = {
             .attachFiles(["./resources/hussein.png"])
             .setThumbnail("attachment://hussein.png")
             .setColor("BLUE")
-            .addField("Current Version", "2.9.0", true)
+            .addField("Current Version", "3.0.0", true)
             .addField("GitHub Repository", "https://github.com/so-andrew/hussein-bot/", true);
         message.channel.send({embed: embed});
     }
