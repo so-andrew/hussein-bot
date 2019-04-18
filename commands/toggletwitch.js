4e4f53fcae60ea12817ddeb471e36dfbfbdea9fa
Glitch (so-andrew-hussein-bot) <none>
2019-04-18 22:57:02 +0000

    Merge conflicts

--- commands/toggletwitch.js
+++ commands/toggletwitch.js
@@ -4,23 +4,24 @@ module.exports = {
     category: "twitch",
     params: "None",
     cooldown: 3,
-    async execute(message){
+    execute(message){
         console.log(`Command ${module.exports.name} received from ${message.author.username}`);
         if(message.client.guildPrefs.has(`${message.guild.id}`)){
             const guildPrefs = message.client.guildPrefs.get(`${message.guild.id}`);
-            const roleExists =  await message.guild.roles.find(role => role.id === guildPrefs.notificationRole);
+            const roleExists = message.guild.roles.has(`${guildPrefs.notificationRole}`);
             if(!roleExists){
                 console.log("Notification role no longer exists.");
                 return message.channel.send("The notification role may no longer exist, please tell a server admin to run `!config` or `!settwitchrole` again.");
             }
-            if(message.member.roles.has(`${roleExists.id}`)){
-                message.member.removeRole(roleExists);
-                console.log(`Removing role ${roleExists.name} from ${message.member.user.username}.`);
+		const role = message.guild.roles.get(`${guildPrefs.notificationRole}`);
+            if(message.member.roles.has(`${role.id}`)){
+                message.member.removeRole(role);
+                console.log(`Removing role ${role.name} from ${message.member.user.username}.`);
                 return message.channel.send("You will no longer receive Twitch live notifications.");
             }
             else{
-                message.member.addRole(roleExists);
-                console.log(`Assigning role ${roleExists.name} to ${message.member.user.username}.`);
+                message.member.addRole(role);               
+		console.log(`Assigning role ${role.name} to ${message.member.user.username}.`);
                 return message.channel.send("You will now receive Twitch live notifications.");
             }
         }
