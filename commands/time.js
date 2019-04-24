70cfb1c218529e0db85b0e5c3e6587f222bd9fb8
Glitch (so-andrew-hussein-bot) <none>
2019-04-24 03:19:10 +0000

    👰🍰 Checkpoint ./commands/time.js:3940672/436

--- commands/time.js
+++ commands/time.js
@@ -4,17 +4,15 @@ module.exports = {
     category: "utility",
     execute(message){
         console.log(`Command ${module.exports.name} received from ${message.author.username}`);
-        let currentHour = message.createdAt.getHours();
+        const d = new Date();
+        message.channel.send(`UTC Offset: ${-1*d.getTimezoneOffset()/60}`);
+        /*let currentHour = message.createdAt.getHours();
         let PM = false;
         if(currentHour > 12){
           currentHour -= 12;
           PM = true;
         }
 
-        const d = new Date();
-        console.log(d.getTimezoneOffset());
-        console.log(message.guild.region);
-      
         let timeString = `${currentHour}:${message.createdAt.getMinutes()}`
         if(PM === true){
           timeString+= " PM"
@@ -27,6 +25,6 @@ module.exports = {
         else if(message.createdAt.getHours() >= 6 && message.createdAt.getHours() <= 9){
           message.reply(`it is ${timeString}. It's way too early for this.`);
         }
-        else message.reply(`it is ${timeString}.`);
+        else message.reply(`it is ${timeString}.`);*/
     }
 }
